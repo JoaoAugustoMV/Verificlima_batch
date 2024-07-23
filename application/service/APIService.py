3c4f7d58571bab8b5c21b5d715e14eab3d3b55a9
JoaoAugustoMV <93055242+JoaoAugustoMV@users.noreply.github.com>
2024-07-23 16:51:51 -0300

    Add more time to timeout
    Azure databases at the free level close connections after 30 minutes of non-use, so it takes more time to return the request response

--- application/service/APIService.py
+++ application/service/APIService.py
@@ -7,6 +7,6 @@ class APIService():
 
     async def insert_infos(self, infos: list[InformationDayForecast]) -> None:
         async with httpx.AsyncClient() as client:
-            resp = await client.post(f'{self.urlBase}/info_dia_temp', json=infos)
+            resp = await client.post(f'{self.urlBase}/info_dia_temp', json=infos, timeout=60)
 
         return resp
